fe2d834943f214cf8dd3861d9638b6ef91d2aa8d
Enoch Atuahene <eatuahene@springfieldcollege.edu>
2024-09-05 13:03:19 -0500

    Cards component started

---
+++ src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='w-full py-40 px-4 bg-white'>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default Cards
